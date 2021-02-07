ecc758360487b7ac198e769cdb01cebbfa1a3afe
Greg Uctum <greguctum@gmail.com>
2021-02-07 17:32:41 -0500

    optimized imports

--- src/main/java/com/uctum/common/utils/service/ChuckService.java
+++ src/main/java/com/uctum/common/utils/service/ChuckService.java
@@ -2,11 +2,7 @@ package com.uctum.common.utils.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Service
 public class ChuckService {
